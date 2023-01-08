5b533ab89329c6b8cb50f2f5ae53b66555857185
Hung Chun Fai <arkon.digital.hcf@gmail.com>
2023-01-08 17:06:54 +0800

    fine-tuned ray-marching technique

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import { loadHDRI } from "./common-utils"
 // Other deps
 const hdriURL = 'https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/empty_warehouse_01_1k.hdr'
 const displacementMapURL = 'https://i.imgur.com/L1pqRg9.jpeg'
-import TextureImg from "./assets/texture.jpeg"
+import TextureImg from "./assets/wavy3.png"
 
 global.THREE = THREE
 
@@ -22,13 +22,13 @@ global.THREE = THREE
 const params = {
   // general scene params
   roughness: 0.1,
-  iterations: 48,
-  depth: 0.6,
+  iterations: 60,
+  depth: 0.3,
   smoothing: 0.2,
   displacement: 0.1,
   speed: 0.05,
   colorA: '#000000',
-  colorB: '#00ffaa'
+  colorB: '#e000ff'
 }
 const uniforms = {
   ...getDefaultUniforms(),
@@ -38,7 +38,6 @@ const uniforms = {
   colorA: { value: new THREE.Color(params.colorA) },
   colorB: { value: new THREE.Color(params.colorB) },
   displacement: { value: params.displacement },
-  time: { value: 0 }
 }
 
 /**************************************************
@@ -85,18 +84,14 @@ let app = {
     // set up environment
     const envMap = await loadHDRI(hdriURL)
     scene.environment = envMap
-
-    const geometry = new THREE.SphereGeometry(1, 64, 32)
-    const material = new THREE.MeshStandardMaterial({ roughness: params.roughness })
+    scene.background = new THREE.Color(0xffffff)
 
     // Load heightmap and displacementMap textures
     const heightMap = await this.loadTexture(TextureImg)
     const displacementMap = await this.loadTexture(displacementMapURL)
     displacementMap.wrapS = displacementMap.wrapT = THREE.RepeatWrapping
-
     // Prevent seam introduced by THREE.LinearFilter
     heightMap.minFilter = THREE.NearestFilter
-
     // Add heightmap and displacementMap to local uniforms object
     uniforms.heightMap = {
       value: heightMap
@@ -105,24 +100,42 @@ let app = {
       value: displacementMap
     }
 
-    material.onBeforeCompile = shader => {
-      // Wire up local uniform references
-      shader.uniforms = { ...shader.uniforms, ...uniforms }
-
-      // Add to top of vertex shader
-      shader.vertexShader = `
+    const auroraGeometry = new THREE.SphereGeometry(1, 64, 32)
+    const baseGeometry = new THREE.SphereGeometry(0.925, 64, 32)
+    auroraMaterial = new THREE.ShaderMaterial({
+      vertexShader: `
+        varying float a_pos;
         varying vec3 v_pos;
         varying vec3 v_dir;
-      ` + shader.vertexShader
+        varying vec3 v_cam;
 
-      // Assign values to varyings inside of main()
-      shader.vertexShader = shader.vertexShader.replace(/void main\(\) {/, (match) => match + `
-        v_dir = position - cameraPosition; // Points from camera to vertex
-        v_pos = position;
-      `)
+        #define PI 3.14159265359
 
-      // Add to top of fragment shader
-      shader.fragmentShader = `
+        // angle returned in radians
+        float angleBetweenVs(vec3 v1, vec3 v2) {
+          return acos(dot(v1, v2) / (length(v1) * length(v2)));
+        }
+
+        void main() {
+          // cal the attackAngle
+          v_pos = position;
+          v_cam = cameraPosition;
+          v_dir = position - cameraPosition; // Points from camera to vertex
+          // vec3 revCam = cameraPosition * -1.;
+          a_pos = angleBetweenVs(cameraPosition, position);
+          // float angleCam = angleBetweenVs(v_dir, revCam);
+          // attackAngle = PI / 2. - anglePos - angleCam;
+
+          gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0); 
+        }
+      `,
+      fragmentShader: `
+        #ifdef GL_ES
+        precision mediump float;
+        #endif
+
+        #include <common>
+
         #define FLIP vec2(1., -1.)
 
         uniform vec3 colorA;
@@ -133,14 +146,16 @@ let app = {
         uniform float depth;
         uniform float smoothing;
         uniform float displacement;
-        uniform float time;
+
+        uniform vec2 u_resolution;
+        uniform vec2 u_mouse;
+        uniform float u_time;
         
         varying vec3 v_pos;
         varying vec3 v_dir;
-      ` + shader.fragmentShader
+        varying vec3 v_cam;
+        varying float a_pos;
 
-      // Add above fragment shader main() so we can access common.glsl.js
-      shader.fragmentShader = shader.fragmentShader.replace(/void main\(\) {/, (match) => `
         /**
          * @param p - Point to displace
          * @param strength - How much the map can displace the point
@@ -148,7 +163,7 @@ let app = {
          */
         vec3 displacePoint(vec3 p, float strength) {
         	vec2 uv = equirectUv(normalize(p));
-          vec2 scroll = vec2(time, 0.);
+          vec2 scroll = vec2(u_time, 0.);
           vec3 displacementA = texture(displacementMap, uv + scroll).rgb; // Upright
 					vec3 displacementB = texture(displacementMap, uv * FLIP - scroll).rgb; // Upside down
           
@@ -159,54 +174,110 @@ let app = {
           return p + strength * (displacementA + displacementB);
         }
 
-				/**
-          * @param rayOrigin - Point on sphere
-          * @param rayDir - Normalized ray direction
-          * @returns Diffuse RGB color
-          */
-        vec3 marchMarble(vec3 rayOrigin, vec3 rayDir) {
-          float perIteration = 1. / float(iterations);
-          vec3 deltaRay = rayDir * perIteration * depth;
-
-          // Start at point of intersection and accumulate volume
-          vec3 p = rayOrigin;
-          float totalVolume = 0.;
-
-          for (int i=0; i<iterations; ++i) {
-            // Read heightmap from spherical direction of displaced ray position
-            vec3 displaced = displacePoint(p, displacement);
-            vec2 uv = equirectUv(normalize(displaced));
-            float heightMapVal = texture(heightMap, uv).r;
-
-            // Take a slice of the heightmap
-            // float height = length(p); // 1 at surface, 0 at core, assuming radius = 1
-            float cutoff = 1. - float(i) * perIteration;
-            float slice = smoothstep(cutoff, cutoff + smoothing, heightMapVal);
-
-            // Accumulate the volume and advance the ray forward one step
-            totalVolume += slice * perIteration;
-            p += deltaRay;
+        // https://en.wikipedia.org/wiki/Quadratic_equation
+        // we are only taking the larger result as we use + sign
+        // but actually we might want to consider both intersections as that's more realistic
+        void solveQuadratic(float a, float b, float c, inout float roots[3], float a_pos) {
+          float discriminant = b*b - 4.0*a*c;
+          if (discriminant > 0.0 && a_pos > 0.0) {
+            roots[0] = 2.0;
+            roots[1] = (-b + sqrt(pow(b,2.0) - 4.0*a*c)) / (2.0 * a);
+            roots[2] = (-b - sqrt(pow(b,2.0) - 4.0*a*c)) / (2.0 * a);
+          } else if (discriminant >= 0.0) {
+            roots[0] = 1.0;
+            roots[1] = (-b + sqrt(pow(b,2.0) - 4.0*a*c)) / (2.0 * a);
+          } else {
+            roots[0] = 0.0;
           }
-          // a lower total volume means closer to colorA (which is supposed to be a darker color)
-          return mix(colorA, colorB, totalVolume);
         }
-      ` + match)
 
-      shader.fragmentShader = shader.fragmentShader.replace(/vec4 diffuseColor.*;/, `
-      	vec3 rayDir = normalize(v_dir);
-        vec3 rayOrigin = v_pos;
-        
-        vec3 rgb = marchMarble(rayOrigin, rayDir);
-				vec4 diffuseColor = vec4(rgb, 1.);
-      `)
-    }
+        // From: https://www.shadertoy.com/view/Ws3Xzr
+        // effect could still be improved, I don't want hard edges for the aurora
+        float smoothClamp(float x, float a, float b)
+        {
+            return smoothstep(0., 1., (x - a)/(b - a))*(b - a) + a;
+        }
+        float softClamp(float x, float a, float b)
+        {
+            return smoothstep(0., 1., (2./3.)*(x - a)/(b - a) + (1./6.))*(b - a) + a;
+        }
 
-    this.mesh = new THREE.Mesh(geometry, material)
-    scene.add(this.mesh)
+        // angle returned in radians
+        float angleBetweenVs(vec3 v1, vec3 v2) {
+          return acos(dot(v1, v2) / (length(v1) * length(v2)));
+        }
+
+        void main() {
+          vec3 rayDir = normalize(v_dir);
+          float radius = 1.0;
+          float totalVolume = 0.;
+          float perIteration = 1. / float(iterations);
+          int heightmapHits = 0;
+
+          // This is to make depth thinner at the fringes of the sphere
+          // such that the shells/iterations are more compact,
+          // if the shells are slightly too far apart, you will see the cross-section lines
+          float m_depth = depth * (1.0 - smoothstep(0.0, 1.15, a_pos));
+
+          // https://en.wikipedia.org/wiki/Line%E2%80%93sphere_intersection
+          // calculate a,b,c of the line equation for v_dir
+          for (int i=0; i<iterations; ++i) {
+            float a = dot(rayDir, rayDir);
+            float b = 2.0 * (dot(rayDir, v_cam));
+            float c = dot(v_cam, v_cam) - pow(radius, 2.0);
+            float roots[3];
+            // solving for intersection points
+            solveQuadratic(a, b, c, roots, a_pos);
+
+            // loop through all intersections, roots[0] stores number of intersections
+            for (int j=1; j<=int(roots[0]); ++j) {
+              vec3 p = v_cam + roots[j] * rayDir;
+              float ang_p = angleBetweenVs(v_cam, p);
+              
+              vec2 uv = equirectUv(normalize(p));
+              float heightMapVal = texture(heightMap, uv).r;
+              if (j == 2) {
+                heightMapVal *= 0.4;
+              }
+
+              if (heightMapVal > 0.0) {
+                heightmapHits += 1;
+              }
+              
+              // Accumulate the volume and advance the ray forward one step
+              totalVolume += heightMapVal * perIteration;
+            }
+            // descend one shell downwards
+            radius -= m_depth * perIteration;
+          }
+          // I want to increase the color for the aurora bands at the fringes
+          // so I use another clamp here for places where bands exist
+          // TODO: could improve this to target just the fringes i guess?
+          if (heightmapHits > 0) {
+            totalVolume = softClamp(totalVolume, 0.15, 0.9);
+          }
+          
+          // Top-clamp the totalVolume so the colors at overlapping areas won't be too blown-up
+          vec4 rgba = mix(vec4(colorA, 0.0), vec4(colorB, 1.0), clamp(totalVolume, 0.0, 0.9));
+          gl_FragColor = rgba;
+        }
+      `,
+      uniforms: uniforms,
+      // transparent: true
+    })
+    const baseMaterial = new THREE.MeshStandardMaterial({
+      roughness: params.roughness,
+      color: new THREE.Color(0x2299dd)
+    })
+
+    this.auroraMesh = new THREE.Mesh(auroraGeometry, auroraMaterial)
+    scene.add(this.auroraMesh)
+    // this.baseMesh = new THREE.Mesh(baseGeometry, baseMaterial)
+    // scene.add(this.baseMesh)
 
     // GUI controls
     const gui = new dat.GUI()
-    gui.add(params, 'roughness', 0, 1, 0.01).onChange(v => material.roughness = v)
+    gui.add(params, 'roughness', 0, 1, 0.01).onChange(v => baseMaterial.roughness = v)
     gui.add(params, 'iterations', 0, 64, 1).onChange(v => uniforms.iterations.value = v)
     gui.add(params, 'depth', 0, 1, 0.01).onChange(v => uniforms.depth.value = v)
     gui.add(params, 'smoothing', 0, 1, 0.01).onChange(v => uniforms.smoothing.value = v)
@@ -227,8 +298,6 @@ let app = {
   updateScene(interval, elapsed) {
     this.controls.update()
     this.stats1.update()
-
-    uniforms.time.value += interval * params.speed
   }
 }
 
